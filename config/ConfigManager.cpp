9bfbce1880364d669e428bf973b24188fb046cf4
Nils Weigel <nils.weigel@gmail.com>
2020-09-17 06:59:24 +0200

    initial work on config manager

---
+++ config/ConfigManager.cpp
@@ -0,0 +1,203 @@
+#include <esp_log.h>
+#include "ConfigManager.h"
+
+namespace EBLi {
+
+#define NVS_NAMESPACE "ebli_config"
+static const char *LOG_TAG = "EBLi:ConfigManager";
+
+// NOTE keys are limited to 15 characters
+#define MAXIMUM_CONFIG_KEY_LENGTH 15
+#define MAXIMUM_CONFIG_VALUE_LENGTH 64
+#define RESTART_COUNTER_KEY "restart_counter"
+
+bool ConfigManager::open()
+{
+    if (!openNvs()) {
+        return false;
+    }
+
+    logNvsState();
+    
+    updateRestartCounter();
+    return true;
+}
+
+bool ConfigManager::openNvs()
+{
+    esp_err_t err = nvs_open(NVS_NAMESPACE, NVS_READWRITE, &m_nvsHandle);
+    if (err != ESP_OK) {
+        ESP_LOGE(LOG_TAG, "Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+        return false;
+    }
+
+    return true;
+}
+
+void ConfigManager::logNvsState()
+{
+    nvs_stats_t nvs_stats;
+    nvs_get_stats(nullptr, &nvs_stats);
+    ESP_LOGI(LOG_TAG, "NVS: UsedEntries = (%d), FreeEntries = (%d), AllEntries = (%d)",
+             nvs_stats.used_entries, nvs_stats.free_entries, nvs_stats.total_entries);
+}
+
+void ConfigManager::close()
+{
+    nvs_close(m_nvsHandle);
+}
+
+void ConfigManager::setAutoCommitEnabled(bool enabled)
+{
+    m_autoCommitEnabled = enabled;
+}
+
+bool ConfigManager::isAutoCommitEnabled() const
+{
+    return m_autoCommitEnabled;
+}
+
+bool ConfigManager::commit()
+{
+    esp_err_t err = nvs_commit(m_nvsHandle);
+    
+    if (err != ESP_OK) {
+        ESP_LOGE(LOG_TAG, "Error (%s) committing NVS Data!", esp_err_to_name(err));
+        return false;
+    }
+
+    return true;
+}
+
+int32_t ConfigManager::getValue(const std::string &key, int32_t defaultValue) const
+{
+    if (!checkKeyAndLog(key)) {
+        return 0;
+    }
+
+    int32_t value = defaultValue;
+    esp_err_t err = nvs_get_i32(m_nvsHandle, key.c_str(), &value);
+
+    if (err != ESP_OK) {
+        ESP_LOGW(LOG_TAG, "NVS read Error (%s), key:\"%s\"", esp_err_to_name(err), key.c_str());
+    }
+    
+    return value;
+}
+
+bool ConfigManager::setValue(const std::string &key, int32_t value)
+{
+    if (!checkKeyAndLog(key)) {
+        return false;
+    }
+
+    esp_err_t err = nvs_set_i32(m_nvsHandle, key.c_str(), value);
+
+    if (err != ESP_OK) {
+        ESP_LOGE(LOG_TAG, "NVS write Error (%s), key:\"%s\"", esp_err_to_name(err), key.c_str());
+        return false;
+    }
+
+    return autoCommit();
+}
+
+std::string ConfigManager::getValue(const std::string &key, const std::string &defaultValue) const
+{
+    if (!checkKeyAndLog(key)) {
+        return "";
+    }
+    if (defaultValue.length() > MAXIMUM_CONFIG_VALUE_LENGTH) {
+        ESP_LOGW(LOG_TAG, "Default value for \"%s\" is too long", key.c_str());
+        return "";
+    }
+
+    size_t required_size;
+    esp_err_t err = nvs_get_str(m_nvsHandle, key.c_str(), nullptr, &required_size);
+    if (err != ESP_OK) {
+        ESP_LOGW(LOG_TAG, "NVS read Error (%s), key:\"%s\"", esp_err_to_name(err), key.c_str());
+        return defaultValue;
+    }
+    if (required_size >= 64) {
+        ESP_LOGW(LOG_TAG, "NVS read failed - max size exceeded, key:\"%s\"", key.c_str());
+        return defaultValue;
+    }
+
+    char* value = (char*)malloc(required_size);
+    err = nvs_get_str(m_nvsHandle, key.c_str(), value, &required_size);
+    if (err != ESP_OK) {
+        ESP_LOGW(LOG_TAG, "NVS read Error (%s), key:\"%s\"", esp_err_to_name(err), key.c_str());
+        return defaultValue;
+    }
+
+    auto ret = std::string(value);
+    free(value);
+    return ret;
+}
+
+bool ConfigManager::setValue(const std::string &key, const std::string &value)
+{
+    if (!checkKeyAndLog(key)) {
+        return false;
+    }
+    if (value.length() > MAXIMUM_CONFIG_VALUE_LENGTH) {
+        ESP_LOGW(LOG_TAG, "Config value for \"%s\" is too long", key.c_str());
+        return false;
+    }
+
+    esp_err_t err = nvs_set_str(m_nvsHandle, key.c_str(), value.c_str());
+    if (err != ESP_OK) {
+        ESP_LOGE(LOG_TAG, "NVS write Error (%s), key:\"%s\"", esp_err_to_name(err), key.c_str());
+        return false;
+    }
+
+    return autoCommit();
+}
+
+int32_t ConfigManager::getRestartCounter()
+{
+    if (m_restartCounter == 0) {
+        m_restartCounter = getValue(RESTART_COUNTER_KEY, 0);
+    }
+    
+    return m_restartCounter;
+}
+
+bool ConfigManager::isKeyValid(const std::string &key)
+{
+    return key.length() <= MAXIMUM_CONFIG_KEY_LENGTH;
+}
+
+bool ConfigManager::checkKeyAndLog(const std::string &key)
+{
+    if (isKeyValid(key)) {
+        return true;
+    }
+
+    ESP_LOGW(LOG_TAG, "Config key \"%s\" is too long", key.c_str());
+    return false;
+}
+
+bool ConfigManager::autoCommit()
+{
+    if (!isAutoCommitEnabled()) {
+        return true;
+    }
+    
+    return commit();
+}
+
+void ConfigManager::updateRestartCounter()
+{
+    if (m_restartCounter > 0) {
+        // already incremented...
+        return;
+    }
+
+    int32_t newCount = getRestartCounter() + 1;
+    if (setValue(RESTART_COUNTER_KEY, newCount)) {
+        ESP_LOGI(LOG_TAG, "Restart counter updated to %d", newCount);
+        m_restartCounter = newCount;
+    }
+}
+
+}
---
+++ config/public/ConfigManager.h
@@ -0,0 +1,53 @@
+#ifndef EBLI_CONFIGMANAGER_H
+#define EBLI_CONFIGMANAGER_H
+
+#include <nvs.h>
+#include <string>
+
+namespace EBLi {
+
+class ConfigManager
+{
+public:
+    bool open();
+    void close();
+    bool commit();
+
+    enum ConfigOptions {
+        NoOptions,
+        AutoPublishMqtt,   // publish change of value via MQTT "deviceTopic/config/KEY"
+        AutoSubscribeMqtt, // allow change of value via MQTT "deviceTopic/config/KEY" and  "groupTopic/config/KEY"
+        // TODO same for REST
+    };
+    void createConfig(const std::string &key, options);
+
+    void setAutoCommitEnabled(bool enabled = true);
+    bool isAutoCommitEnabled() const;
+
+    int32_t getValue(const std::string &key, int32_t defaultValue) const;
+    bool setValue(const std::string &key, int32_t value);
+
+    std::string getValue(const std::string &key, const std::string &defaultValue) const;
+    bool setValue(const std::string &key, const std::string &value);
+
+    int32_t getRestartCounter();
+
+protected:
+    bool openNvs();
+    void logNvsState();
+
+    static bool isKeyValid(const std::string &key);
+    static bool checkKeyAndLog(const std::string &key);
+
+    bool m_autoCommitEnabled = true;
+    bool autoCommit();
+
+    nvs_handle_t m_nvsHandle = 0;
+
+    void updateRestartCounter();
+    int32_t m_restartCounter = 0;
+};
+
+}
+
+#endif //EBLI_CONFIGMANAGER_H
